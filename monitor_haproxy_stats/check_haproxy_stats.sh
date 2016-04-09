aa52cc04efe76b7584d7efadbda3069f2e58844d
rockneu <rock.neu@gmail.com>
2016-04-09 14:05:25 +0800

    updatg check_haproxy_stats.sh

--- monitor_haproxy_stats/check_haproxy_stats.sh
+++ monitor_haproxy_stats/check_haproxy_stats.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+# This script is designed to work as a nagios plugin, monitoring HAProxy service statistics.
+#
+# Precondition
+# 1) need socat to be installed at the haproxy server to be monitored
+# 2) need to configure socket at haproxy.cfg, default at /var/run/haproxy.sock
+#
+#
+# Usage:
+#	./check_haproxy_stats.sh type service_name
+#		type could be frontend or backend
+#		service_name is the frontend name or backend name, not the server name.
+#	eg:		./check_haproxy_stats.sh frontend web-service
+#
+#
+# Author: Rock Liu 20160409
+#
+# Updating history
+#
+#
 
- 
-#variables
+
 ptype="$1"
 pname="$2"
-tmp_file='/tmp/ha_stat.txt'
+pct="$#"
+
+temp_file='/tmp/ha_stat.txt'
 stat_file='/var/run/haproxy.sock'
 
 #msg
@@ -40,46 +60,32 @@ response_out="0"
 
 # get haproxy stats info and dump to local file
 dump_info() {
-	#/bin/echo "show info;show stat" | /usr/bin/nc -U /var/run/haproxy.sock  > $tmp_file
+	#/bin/echo "show info;show stat" | /usr/bin/nc -U /var/run/haproxy.sock  > $temp_file
 	#/bin/echo "show info;show stat" | /usr/local/bin/socat unix-connect:/var/run/haproxy.sock stdio > /tmp/ha_stat.txt
-	/bin/echo "show info;show stat" | /usr/local/bin/socat unix-connect:$stat_file stdio > $tmp_file
+	/bin/echo "show info;show stat" | /usr/local/bin/socat unix-connect:$stat_file stdio > $temp_file
 }
 
-print_help() {
-#	echo "Usage: $0 {byte_in|byte_out|curr_conns|cum_conns|cum_http|http_1xx|http_2xx|http_3xx|http_4xx|http_5xx}"
 
-#	to get data: status, byte_in/s, byte_out/s, [request/s response/s]
-#	frontend: 		status,uptime, byte_in/s, byte_out/s, request/s
-#	backend:		status,uptime, byte_in/s, byte_out/s, response/s
-	#backend.node	status,uptime, byte_in/s, byte_out/s, response/s
 
-	
-	echo "Usage: ./check_haproxy_stat.sh node_type node_name"
-	echo " node_type: frontend | backend"
-	echo " node_name: the name of nodes" 
-	echo " 		./check_haproxy_stat.sh frontend mail-web"
-	echo " 		./check_haproxy_stat.sh frontend web-service"
-	echo " 		./check_haproxy_stat.sh backend node.51"
-
-	
-	}
 
 judge() {
 
 	unit=""
 	
 	if [ $status = "OPEN" ] || [ $status = "UP" ]; then
-		msg="HAProxy Service OK : "
+		msg="HAProxy Service OK: "
 	else
-		msg="haproxy Server Critical :"
+		msg="haproxy Server Critical:"
 	fi
 
-	if [ "$p1" = "frontend" ]; then
+	if [ "$ptype" = "frontend" ]; then
 		rate_in=`echo "$byte_in / $time_up" | bc`
 		rate_out=`echo "$byte_out / $time_up" | bc`
 		rate_req=`echo "$request_in / $time_up" |bc`
 	fi
 
+	#echo "$rate_in $rate_out $rate_req"
+	
 	if [ "$rate_in" -lt "1024" ];then
 		unit="bps"
 		rate_in="$rate_in$unit"
@@ -113,25 +119,50 @@ judge() {
 
 extract_data() {
 
-#	cat $tmp_file | awk -F: '/app-sso/' | awk -F: '/BACKEND/'
-#	time_up=`cat $tmp_file | awk -F, '{print $24}'`
+#	cat $temp_file | awk -F: '/app-sso/' | awk -F: '/BACKEND/'
+#	time_up=`cat $temp_file | awk -F, '{print $24}'`
 #	/bin/echo "show stat" | /usr/bin/nc -U  /var/run/haproxy.sock | awk -F, '{print $24}'
 
-	if [ "$p1" = "frontend" ]; then
-		status=`cat $tmp_file | awk -F, '/web-service/' | awk -F, '{print $18}'`
-		time_up=`cat $tmp_file | awk -F: '/Uptime_sec/' | awk -F: '{print $2}'`
-		byte_in=`cat $tmp_file | awk -F, '/web-service/' | awk -F, '{print $9}'`
-		byte_out=`cat $tmp_file | awk -F, '/web-service/' | awk -F, '{print $10}'`
-		request_in=`cat $tmp_file | awk -F, '/web-service/' | awk -F, '{print $8}'`
+	if [ "$ptype" = "frontend" ]; then
+		status=`cat $temp_file | awk -F, /"$pname"/ | awk -F, '{print $18}'`
+		time_up=`cat $temp_file | awk -F: /'Uptime_sec'/ | awk -F: '{print $2}'`
+		byte_in=`cat $temp_file | awk -F, /"$pname"/ | awk -F, '{print $9}'`
+		byte_out=`cat $temp_file | awk -F, /"$pname"/ | awk -F, '{print $10}'`
+		request_in=`cat $temp_file | awk -F, /"$pname"/ | awk -F, '{print $49}'`
 	else
 		echo "not in if branch"
 	fi
+
 	
 }
 
-dump_info
-       
-# extract some data here
-extract_data
+check_input() {
 
-judge
+	if [ "$pct" != 2 ];then
+		echo "Incorrect parameter count. 2 parameter needed!"
+		return 2
+	fi
+
+	if [ "$ptype" != "frontend" -a "$ptype" != "backend" ];then
+		echo "Incorrect type parameter. 'frontend' or 'backend' needed!"
+		return 2
+	fi
+
+	return 1
+}
+
+
+dump_info
+
+check_input
+ret=$?
+#echo "ret=$ret"
+
+if [ "$ret" == "1" ];then
+	# extract some data here
+	extract_data
+	judge	
+else
+	echo "check_input failed!"
+fi
+       
